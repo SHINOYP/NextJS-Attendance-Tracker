5c978414fa13d1103a4bf2bbe05e4ab8ccd997e3
SHINOYP <shinoyshinoo@gmail.com>
2025-03-11 23:24:10 +0530

    Add ProtectedRoute component and update layout for session management; extend User model and add Session and Attendance models in Prisma schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./components/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main>
-          <AuthProvider>{children}</AuthProvider>
+          <AuthProvider>
+            <ProtectedRoute>
+              {children}
+            </ProtectedRoute>
+          </AuthProvider>
         </main>
       </body>
     </html>
